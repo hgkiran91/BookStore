f0f18440e9236d63b2e55b4e7e9baaf822cfed0e
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-08-03 16:18:30 +0530

    [Kiran] Added: Initial commit

---
+++ src/models/user.model.js
@@ -0,0 +1,15 @@
+import { optional, string } from '@hapi/joi';
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default model('User', userSchema);
